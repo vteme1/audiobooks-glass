51776c387f13af2cc1df119b5f964b9c615a469b
vteme1 <62310457+vteme1@users.noreply.github.com>
2025-08-10 08:08:48 +0300

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,5 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
+const { pathToFileURL } = require('url');
 
 contextBridge.exposeInMainWorld('electron', {
-  openFiles: async () => ipcRenderer.invoke('dialog:openFiles'),
+  openFiles: () => ipcRenderer.invoke('dialog:openFiles'),
+  toFileUrl: (p) => pathToFileURL(p).href
 });
